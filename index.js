3ec75c736fe9cd744973e73817aafaf02d019e1b
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-03-13 13:29:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet<=400){
+    return 'This one is on me!';
+  } else if (feet>2000 && feet<=2500) {
+return 'I will gladly take your thirty bucks.'
+  }
+  else if (feet>2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  
+  return city==='NYC' ? 'Ok, sounds good.': 'No go.';
 }
+function switchOnCharmFromTip(tipAmount) {
+  let response;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tipAmount) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+      break;
+  }
+
+  return response;
 }
